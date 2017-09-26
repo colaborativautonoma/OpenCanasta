552363d0d5eabd262d19ecda1eb6a0040031138a
David Higuita <david.higuita.75@gmail.com>
2017-09-26 18:10:26 -0500

    Modified server.js to serve static file on node.js for heroku

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use('/', express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
